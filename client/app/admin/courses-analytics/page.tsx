8664151ea8a1fb14727cb682593b9c62287017e5
drgerishon <gerishonkungu@gmail.com>
2024-01-28 15:35:02 +0300

    set open error solving

--- client/app/admin/courses-analytics/page.tsx
+++ client/app/admin/courses-analytics/page.tsx
@@ -1,35 +1,33 @@
-'use client'
-import React, {FC, useState } from 'react'
-import Heading from '../../../app/utils/Heading'
-import DashboardHeader from '../../../app/components/Admin/DashboardHeader'
-import AdminSidebar from '@/app/components/Admin/sidebar/AdminSidebar'
-import CourseAnalytics from '@/app/components/Admin/course/CourseAnalytics'
-
+'use client';
+import React, { FC, useState } from 'react';
+import Heading from '../../../app/utils/Heading';
+import DashboardHeader from '../../../app/components/Admin/DashboardHeader';
+import AdminSidebar from '@/app/components/Admin/sidebar/AdminSidebar';
+import CourseAnalytics from '@/app/components/Admin/course/CourseAnalytics';
 
 type Props = {
-  setOpen:any;
-}
+  setOpen: any;
+};
 
-const Page:FC<Props> = ({setOpen}) => {
-
+const Page: FC<Props> = ({ setOpen }) => {
   return (
     <div>
-       <Heading
+      <Heading
         title="Gshon E-Learning"
         keywords="Elearning"
         description="MERN, REACT, NEXT, NODE"
       />
-      <div className='flex'>
-        <div className='1500px:w-[16%] w-[1/5]'>
-            <AdminSidebar />
+      <div className="flex">
+        <div className="1500px:w-[16%] w-[1/5]">
+          <AdminSidebar />
         </div>
-        <div className='w-[85%]'>
-            <DashboardHeader setOpen={setOpen} />
-            <CourseAnalytics />
+        <div className="w-[85%]">
+          <DashboardHeader setOpen={setOpen} />
+          <CourseAnalytics />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
